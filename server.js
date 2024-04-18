eb8d683f6b3f24f06ba14d44d8d69bbb8bff20a8
Aash <31497659+AashPau@users.noreply.github.com>
2024-04-18 21:26:09 +1000

    feat: setup routes for CRUD

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 
-app.get("/api/contacts", (req, res) => {
-  res.json({ message: "Get all contacts" });
-});
+app.use("/api/contacts", require("./routes/contactRoutes"));
 
-app.listen(PORT, () => {
-  console.log(`Server running on http//:localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on http//:localhost:${port}`);
 });
